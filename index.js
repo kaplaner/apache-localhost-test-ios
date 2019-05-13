3c7ecc4af8754a587a50794f00fad1488d0c6f98
Goran Kaplanovic <goran.kaplanovic@gmail.com>
2019-05-13 16:44:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.onload = function() {
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_1mb.mp4' },
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_20mb (1).mp4' },
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_20mb (2).mp4' },
-    { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_20mb (2).mp4' },
+    { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/metaxas-keller-Bell.mp4' },
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_20mb (2).mp4' }
   ];
   // Create an instance of a db object for us to store our database in
