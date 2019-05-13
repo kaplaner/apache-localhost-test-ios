5809ae5ee40ad3b11df58b242277c3fdff578412
Goran Kaplanovic <goran.kaplanovic@gmail.com>
2019-05-13 13:58:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 var filesToCache = [
     // mainFiles: [
-       '/',
-       '/index.html',
-       '/index.js',
-       '/styles.css',
+//        '/',
+       '/apache-localhost-test-ios/sw.js/index.html',
+       '/apache-localhost-test-ios/sw.js/index.js',
+       '/apache-localhost-test-ios/sw.js/styles.css',
     // ]
  // var versionFiles = [
         // "/content/resources/css/mobile-chutes-ui.min.css?v=",
