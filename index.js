a794dade37ab4110a79a4145471ec0158129eb58
Goran Kaplanovic <goran.kaplanovic@gmail.com>
2019-05-15 14:45:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.onload = function() {
   const section = document.querySelector('section');
   const videos = [
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_1mb.mp4' },
+    { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_1mb.mp4' },
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/SampleVideo_1280x720_20mb (1).mp4' },
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/Cloud Formation Video.mp4' },
     { 'url' : 'https://kaplaner.github.io/apache-localhost-test-ios/metaxas-keller-Bell.mp4' },
